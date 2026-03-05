861e8e1d8e7f2acc70d759cff0e19704db05b275
JiwonShon <jwsohn92@gmail.com>
2026-03-05 22:45:25 +0900

    ECGI10: confusing...need to more digging

---
+++ check_SV_parental_ECGI.py
@@ -0,0 +1,3 @@
+# /data/ECTRES/results/aaSuite_germline_ms/v1.3.8/GRCh37/minCN4.5/cnsizeMin50000/-1X/calls/ECTRES-ECGI1-0001-TPX-A01-WGS-1ST985/ECTRES-ECGI1-0001-TPX-A01-WGS-1ST985_output/ECTRES-ECGI1-0001-TPX-A01-WGS-1ST985_classification/ECTRES-ECGI1-0001-TPX-A01-WGS-1ST985_SV_summaries/ECTRES-ECGI1-0001-TPX-A01-WGS-1ST985_amplicon7_SV_summary.tsv
+
+# singularity shell -B /mnt/NAS3/home/jiwon/HL-NF/scratch/ECTRES:/data/ECTRES/ aasuite_v1.3.8.sif
